76cd747aeb7a5434377b85b32b2bd0ca67a87b49
KrishnaPriyaKanduri <120378231+KrishnaPriyaKanduri@users.noreply.github.com>
2023-01-30 14:51:36 +0530

    Add files via upload

---
+++ 1:all operators/alloperators.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a=10,b=20;
+printf("sum of two numbers is %d\n", a+b);
+printf("difference of two numbers is %d\n", a-b);
+printf("%d\n" , a==b);
+printf("%d\n" , a!=b);
+printf("%d\n" , a<=b);
+printf("%d\n" , a>b && a<b);
+printf("%d\n" , a>=b);
+printf("%d\n" , a<b || a>b);
+return 0;
+}
+
